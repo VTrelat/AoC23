66c10e4b22517e1a1a22bb0b2cc79e7cae487f07
Vincent Trélat <vincent.trelat@free.fr>
2023-12-06 07:24:31 +0100

    [day 6] js one-lined + physics

---
+++ solutions/day6_2.mjs
@@ -0,0 +1,2 @@
+import * as fs from "fs";
+console.log(fs.readFileSync("/Users/vincent/Documents/AoC23/inputs/input6.txt", { encoding: "utf-8", flag: "r" }).trim().split("\n").map((l) =>Number(l.split(":")[1].trim().split(" ").filter((w) => w.length > 0).reduce((acc, v) => acc + v, ""))).reduce((t, d) => {const s1 = Math.round(t + Math.sqrt(t * t - 4 * d) / 2);const s2 = Math.round(t - Math.sqrt(t * t - 4 * d) / 2);console.log(s1 - s2);return s1 - s2;}));
